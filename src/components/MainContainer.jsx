4130d274e3b4fb0be53afc1f86ad540e7cb4aa87
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-09-18 15:51:03 -0300

    install swiper dependencies and create carousel component carousel for popular and top rated

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,14 @@
 import CarouselMoviesBanner from "./CarouselMoviesBanner"
+import CarouselPopular from "./CarouselPopular"
+import CarouselTopRated from "./CarouselTopRated"
+
 
 export default function MainContainer() {
     return (
         <main className="flex flex-col flex-grow">
             <CarouselMoviesBanner />
+            <CarouselPopular />
+            <CarouselTopRated />
         </main>
     )
 }
