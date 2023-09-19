66d66ebfe7a23695f21f7f7d599606194d52c461
Rocio-Ch <rociomagali77@gmail.com>
2023-09-19 15:09:50 -0300

    adding DetailMovie component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from './components/Header';
 import MainContainer from './components/MainContainer';
+import DetailMovie from './components/DetailMovie'
 
 export default function App() {
   
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<MainContainer />} />
+        <Route path='/descripcion' element={<DetailMovie />}/>
       </Routes>
     </BrowserRouter>
   );
