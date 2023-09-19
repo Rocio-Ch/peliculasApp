5a93264990478e2c5da390ab12b6da3a5d62d8ae
Rocio-Ch <rociomagali77@gmail.com>
2023-09-19 14:51:05 -0300

    uninstall minimal-carousel dependencies, install swiper dependencies and edit carouselMovieBanner

--- src/components/CarouselMoviesBanner.jsx
+++ src/components/CarouselMoviesBanner.jsx
@@ -1,50 +1,47 @@
-import { Carousel } from "react-carousel-minimal";
+// Import Swiper React components
+/* import React, { useRef, useState } from 'react';
+import { useEffect } from 'react'; */
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 
-export default function CarouselMovies() {
- const data = [
-    {
-      image: "https://blogs.uai.com.br/opipoqueiro/wp-content/uploads/sites/54/2021/10/Duna-01.jpg",
-      caption: "Dune"
-    },
-    {
-      image: "https://neftaleon.files.wordpress.com/2011/06/xmenfirstclassukbanner.jpg",
-      caption: "X-men"
-    },
-    {
-      image: "https://www.wetanz.com/media//amasty/shopby/option_images/146768033123a810f5abad3aa0a1420b83498efab1.jpg",
-      caption: "The lord of the Rings"
-    }
-  ];
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-  const captionStyle = {
-    fontSize: '2.5rem',
-    fontWeight: 'bold',
-    color: 'white',
-    textShadow: '1px 0 15px #000 , -1px 0 15px #000 , 0 1px 15px #000 , 0 -1px 15px #000 , 1px 1px 15px #000 , -1px -1px 15px #000 , 1px -1px 15px #000 , -1px 1px 15px #000 '
-  }
-  
+// import required modules
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+
+export default function CarouselMoviesBanner() {
+
   return (
-    <div className="Carousel">
-      <div style={{ textAlign: "center" }}>
-        <Carousel
-        data={data}
-        time={4000}
-        width="100%"
-        height="600px"
-        captionStyle={captionStyle}
-        captionPosition="bottom"
-        automatic={true}
-        /* dots={true} */
-        slideBackgroundColor="darkgrey"
-        slideImageFit="cover"
-        style={{
-            textAlign: "center",
-            maxWidth: "100%",
-            maxHeight: "600px",
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={'fade'}
+        navigation={true}
+        pagination={{
+          clickable: true,
         }}
-        />
-        <button className="bg-[#000] text-white py-[5px] px-2.5 relative bottom-[35px] text-xs rounded-lg md:px-5 md:bottom-[45px] md:text-lg">Ver detalles</button>
-      </div>
-    </div>
+        autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+        }}
+        modules={[EffectFade, Navigation, Pagination, Autoplay]}
+        loop={true}
+        className="mySwiper relative w-full max-h-[750px] mt-[60px]"
+      >
+        <SwiperSlide className='bg-center bg-cover'>
+                <div className='relative w-full text-white flex justify-center'>
+                    <img src="" className='w-full max-h-[900px] block' />
+                </div>
+                <div className='absolute flex flex-col items-center justify-center text-white w-[90%] bottom-[30px] p-2.5 bg-[#030317b0] rounded lg:p-5 lg:text-[2rem] xl:w-2/5 xl:bottom-10'>
+                      <h1 className='text-sm font-semibold mb-2 md:text-xl xl:mb-4 xl:text-[2rem]'></h1>
+                      <button className='text-xs py-1 px-4 bg-[#007aff] rounded lg:py-1 lg:px-6 lg:bottom-[120px] lg:text-lg'>Ver más</button>
+                </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from "react";
 import CarouselMoviesBanner from "./CarouselMoviesBanner"
 
 export default function MainContainer() {
+
     return (
         <main className="flex flex-col flex-grow">
             <CarouselMoviesBanner />
