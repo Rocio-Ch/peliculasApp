6cf851e3cb7e888567760a83be41d79e31e9426c
Verónica del Mar Guiñan López <veronicadmgl@gmail.com>
2023-09-19 15:14:42 -0300

    create components Latest releases and Popular

---
+++ src/components/LatestReleases.jsx
@@ -0,0 +1,155 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+
+export default function LatestReleases() {
+    return (
+        <section className="mt-[80px] mx-[35px] flex flex-wrap justify-center">
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+            <Link to="/descripcion">
+                <Card className='w-[250px] h-[340px] m-10 hover:transition hover:duration-300 hover:ease-in-out hover:shadow-lg dark:hover:shadow-[0px_5px_30px_0px_rgba(24,17,167,79%)] hover:scale-105'>
+                    <CardMedia
+                        className='h-[300px]'
+                        component="img"
+                        alt="green iguana"
+                        image="https://c4.wallpaperflare.com/wallpaper/750/174/497/animados-cars-cartoon-dibujos-wallpaper-preview.jpg"
+                    />
+                    <Typography sx={{padding: '8px', textAlign:'center', fontWeight:'700', fontSize:'1.1rem'}} component="div">
+                        Cars
+                    </Typography>
+                </Card>
+            </Link>
+        </section>
+    )
+}
